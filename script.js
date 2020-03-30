ffc05fadedf54d2da670f8d03e4ec11f37d3ec0e
Josh Goebel <me@joshgoebel.com>
2020-03-29 21:38:11 -0400

    fix bold and italic keystrokes on mac

--- script.js
+++ script.js
@@ -10,8 +10,31 @@ const addNewSolutionsMenuLink = () => {
     $("<li><a href='/mentor/dashboard/next_solutions'>New solutions</a></li>"))
 }
 
+const getEditor = () => document.querySelector('textarea[name="discussion_post[content]"]');
+
+const fixEditorKeystrokes = () => {
+  let boldButton = document.querySelector('button[data-hotkey="Ctrl+B"]')
+  let italicButton = document.querySelector('button[data-hotkey="Ctrl+I"]')
+  getEditor().addEventListener("keydown", (event) => {
+    let click = new MouseEvent('click', { bubbles: true});
+    if (event.metaKey && event.key=="b") {
+      boldButton.dispatchEvent(click);
+    }
+    if (event.metaKey && event.key=="i") {
+      italicButton.dispatchEvent(click);
+    }
+  })
+
+}
+
+const onMacintosh = () => {
+  return /Macintosh/.test(navigator.userAgent)
+}
+
 const boot = () => {
   addNewSolutionsMenuLink();
+  if (onMacintosh())
+    fixEditorKeystrokes();
 }
 
 boot();
