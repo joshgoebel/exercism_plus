00c5cb6ff13c1ba2d9acb6493727b006ac62ce6f
Josh Goebel <me@joshgoebel.com>
2020-04-07 00:56:39 -0400

    wip2

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,11 @@ export const onMacintosh = () => {
   return /Macintosh/.test(navigator.userAgent)
 }
 
-export const redirect = (url) => window.location.href = url
+export const redirect = (url:string) => window.location.href = url
 
-export function $(html) {
+export function $(html:string) : Element {
   var wrapper = document.createElement('div');
   wrapper.innerHTML = html.trim();
-  return wrapper.firstChild;
+  return (wrapper.firstChild || wrapper) as Element;
 }
 
